adeab17580d3423506e357caf145b9a08e9b1e3c
thebookcheese <160770461+thebookcheese@users.noreply.github.com>
2024-12-10 14:23:26 +0000

    Epic

--- EpicHacking.py
+++ EpicHacking.py
@@ -1,23 +1,46 @@
 import random
 import time
 
-Sentences = ["Installing Package: ", "Hacking World Governments", "Stealing", "Exporting File:"]
+PipInstalled = False
+Sentences = ["Installing Package: ", "Hacking World Governments", "Stealing", "Exporting File:", 'Deleting File', 'Pip Installing']
 Sentences2 = ["'s Personal info"]
+StealingThings = ['Bank Details', 'Address','Passwords']
 Packages = ["HacketyHack", "Random","Pip","HOI4","Totally not virus"]
 PersonNames = ["John", "Dan", "Peter","Joe"]
-Files = ["Sys32","Passwords","SecretSecret"]
+Files = ["Sys32","Passwords","SecretSecret", 'HAHA']
+PipPackages = ['Tkinter', 'Matplotlib']
+InstalledPipPackages = []
 
-for i in range(20):
-    time.sleep(0.1)
+for i in range(50):
+    time.sleep(random.uniform(0.1,0.15))
     Sentence = random.randint(0, len(Sentences))
     if Sentence == 3:
+        ThingToSteal = StealingThings[random.randint(0, len(StealingThings)-1)]
+        Person = PersonNames[random.randint(0,len(PersonNames)-1)]
+        print(Sentences[2], Person + "'s " + ThingToSteal)
+    elif Sentence == 0:
         print(Sentences[1])
     elif Sentence == 2:
-        LengthOfPackages = len(Packages) - 1
+        LengthOfPackages = len(Files) - 1
         PackPick = random.randint(0,LengthOfPackages)
-        print(Sentences[0], Packages[PackPick])
+        print(Sentences[2], Files[PackPick])
     elif Sentence == 1:
-        print(Sentences[3],Files[random.randint(0,len(Files)-1)])
+        File = Packages[random.randint(0, len(Files)-1)]
+        if File == "Pip" and PipInstalled == True:
+            print("Pip Already Installed")
+        else:
+            PipInstalled = True
+            print(Sentences[0],File)
+    elif Sentence == 4:
+        print(Sentences[4], Files[random.randint(0,len(Files)-1)])
+    elif Sentence == 5:
+        if PipInstalled == True:
+            PipPackage = PipPackages[random.randint(0, len(PipPackages) - 1)]
+            if PipPackage in InstalledPipPackages:
+                print(f"{PipPackage} already installed")
+            else:
+                InstalledPipPackages.append(PipPackage)
+                print(Sentences[2], PipPackage)
     else:
         RandomPersonName = random.randint(0,(len(PersonNames)-1))
         print(Sentences[2], PersonNames[RandomPersonName],Sentences2[0])
--- triongle.py
+++ triongle.py
@@ -1,7 +1,16 @@
-x= float(input("input the width of the triangle "))
-y= float(input("input the height of the triangle "))
-result= (x*y)/2
-print("the area of your triangle is", result, "units squared")
+import math
+Want = input('Do you want to work out the area of a triangle or the angles in a triangle').lower()
+if Want == "area":
+  x= float(input("input the width of the triangle "))
+  y= float(input("input the height of the triangle "))
+  result= (x*y)/2
+  print("the area of your triangle is", result, "units squared")
+elif Want == "angles":
+  HaveHypotenuse = input("Do you have the hypotenuse").lower()
+  if HaveHypotenuse == "y":
+    HypoLength = float(input('What is the length of the hypotenuse'))
+    OtherSideLength  = float(input('What is the length of the other side'))
+    print(f'The angle is {OtherSideLength/HypoLength}')
 
 question=input("do you want to return to home? y/n: ")
 
